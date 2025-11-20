887036ff554a798e9f0672f14c8250ad4dc2eb43
boshra-eng <engineerboshra582@gmail.com>
2025-11-20 12:00:03 +0300

    lab2 -Arrays -p1

---
+++ src/Arrays/Arrayss.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class Arrayss {
+    public void R_Traversal(int []a){
+        System.out.println("The elements are:");
+        for (int i=0; i>a h;i--)
+            System.out.println("Index["+i+"]="+a[i]);
+
+    }
+    }
+
