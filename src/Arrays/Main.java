4d206feffc13afe826ae36c8571751023e487a49
boshra-eng <engineerboshra582@gmail.com>
2025-11-20 12:02:51 +0300

    lab2 -Arrays -p11

--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        int [] numbers={1,2,3,4,5};
+        int [] numbers={1,2,3,4,5,6};
         // int [] numbers2= new int[5];
         //  System.out.println(Arrays.toString(numbers2));
         for (int i=0; i<numbers.length;i++)
